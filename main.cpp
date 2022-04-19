48ede7849320b879c0010840d89bb8dcd5d19c9a
Chen Wang <34707694+windwangustc@users.noreply.github.com>
2022-04-19 19:58:40 +0800

    add Function CountMisplacedStars

--- main.cpp
+++ main.cpp
@@ -10,11 +10,33 @@ void A_h1(const vector<vector<int> > &start, const vector<vector<int> > &target)
 void A_h2(const vector<vector<int> > &start, const vector<vector<int> > &target);
 void IDA_h1(const vector<vector<int> > &start, const vector<vector<int> >&target);
 void IDA_h2(const vector<vector<int> > &start, const vector<vector<int> > &target);
+vector<vector<int>> ReadFile(const char* FileName);
+int CountMisplacedStars(const vector<vector<int>> &source, const vector<vector<int>> &target);
+
+int main (int argc,char* argv []){
+    //测试读取txt数据并打印输出
+    vector<vector<int>> SourceMatrix = ReadFile("../data/input00.txt");
+    vector<vector<int>> TargetMatrix = ReadFile("../data/target00.txt");
+    for(int i=0;i<SourceMatrix.size();i++){
+        for (int j = 0; j < SourceMatrix[0].size(); ++j) {
+            cout<<SourceMatrix[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    for(int i=0;i<TargetMatrix.size();i++){
+        for (int j = 0; j < TargetMatrix[0].size(); ++j) {
+            cout<<TargetMatrix[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    //测试输出错位的星球数
+    cout<<"Misplaced Stars = "<<CountMisplacedStars(SourceMatrix,TargetMatrix)<<endl;
+    return 0;
+}
 
 /*读取txt文件内的矩阵，返回一个int[5][5]型的变量*/
-vector<vector<int>> ReadFile(const char* FileName)
-{
-    vector<vector<int>> matrixALL{};  //返回数组
+vector<vector<int>> ReadFile(const char* FileName){
+    vector<vector<int>> sourceMatrix{};  //返回数组
     int row = 5;
     int count = 0;// 行数计数器
     string tmp;   // 暂存文件内单行的字符
@@ -24,8 +46,7 @@ vector<vector<int>> ReadFile(const char* FileName)
         //文件打开失败:返回0
         throw logic_error("Read File Failed");
     }else{
-        while (getline(fileStream, tmp, '\n'))//读取一行
-        {
+        while (getline(fileStream, tmp, '\n')){     //读取一行
             //cout<<tmp<<std::endl;
             vector<int> tmpV{};
             istringstream is(tmp);      //分割每行字符串
@@ -34,23 +55,24 @@ vector<vector<int>> ReadFile(const char* FileName)
                 is>>str_tmp;
                 tmpV.push_back(stoi(str_tmp));
             }
-            matrixALL.push_back(tmpV);
+            sourceMatrix.push_back(tmpV);
             count++;
         }
         fileStream.close();
     }
-    return matrixALL;
+    return sourceMatrix;
 }
 
-int main (int argc,char* argv []){
-    //测试读取txt数据并打印输出
-    vector<vector<int>> matrixALL = ReadFile("../data/input00.txt");
-    cout<<matrixALL.size()<<matrixALL[0].size()<<endl;
-    for(int i=0;i<matrixALL.size();i++){
-        for (int j = 0; j < matrixALL[0].size(); ++j) {
-            cout<<matrixALL[i][j]<<" ";
+//计算错位的星球数，用于启发式函数h1(n)
+int CountMisplacedStars(const vector<vector<int>> &source, const vector<vector<int>> &target){
+    int count=0;
+    //遍历比较source和target不同的数目即可
+    for(int i=0;i<source.size();i++){
+        for (int j = 0; j < source[0].size(); ++j) {
+            if(source[i][j] != target[i][j]){
+                count ++;
+            }
         }
-        cout<<endl;
     }
-    return 0;
+    return count;
 }
